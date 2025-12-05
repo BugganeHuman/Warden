ba96a30c5cbf2c9ab8715c63c6146bd824a95d4f
Elisei <markizdelakarabas@gmail.com>
2025-12-05 17:40:48 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,2 +1,14 @@
 import logics
 import customtkinter
+import sys
+from pathlib import Path
+def main():
+    master_key_input = input("write master key: ")
+    if not Path("salt.key").exists():
+        if len(master_key_input) < 11: 
+            print("minimal length of password - 10 chapters")
+            sys.exit()
+    logics.start(master_key_input)
+
+if __name__ == "__main__":
+    main()
