36f3965db31589c84b93c0a1cfe81886422f49ea
Elisei <markizdelakarabas@gmail.com>
2025-12-05 18:26:54 +0500

    Update main.py

--- main.py
+++ main.py
@@ -11,7 +11,9 @@ def main():
             continue
         print(f"{index} link = {element[0]} | login = {"*" * len(element[1])} | password =  {"*" * len(element[2])}")
         index += 1
+    choice_element = input("\nwrite number of element to watch full: ")
+    list_of_element = logics.show_element_secret_data(int(choice_element))
+    print(f"link ={list_of_element[0]} | login = {list_of_element[1]} | password = {list_of_element[2]}")
 
 if __name__ == "__main__":
     main()
-
