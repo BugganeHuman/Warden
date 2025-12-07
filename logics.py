32200df26091aa1cb2ed2e865dc01264c0f92b34
Elisei <markizdelakarabas@gmail.com>
2025-12-07 15:24:34 +0500

    Update logics.py

--- logics.py
+++ logics.py
@@ -11,31 +11,32 @@ import secrets
 
 
 # мб эти все переменный надо перевести в хай регистр ибо это константы
-conn = sqlite3.connect("vault.db")
-cursor = conn.cursor()
-path_to_salt = Path("salt.key")
-master_password = None
-fernet = None
+CONN = sqlite3.connect("vault.db")
+CURSOR = CONN.cursor()
+PATH_TO_SALT = Path("salt.key")
+MASTER_PASSWORD = None
+FERNET = None
+
 def start(enter_password):
-    global master_password
-    global fernet
-    master_password = enter_password
-    cursor.execute("""
+    global MASTER_PASSWORD
+    global FERNET
+    MASTER_PASSWORD = enter_password
+    CURSOR.execute("""
         CREATE TABLE IF NOT EXISTS vault (
         link BLOB, 
         login BLOB,
         password BLOB
         )
         """)
-    conn.commit()
-    if not path_to_salt.exists():
-        if len(master_password) < 10:
+    CONN.commit()
+    if not PATH_TO_SALT.exists():
+        if len(MASTER_PASSWORD) < 10:
             print("minimal length of password - 10 chapters")
             return
         create_salt()
-        fernet = Fernet(master_key())
+        FERNET = Fernet(master_key())
         create_element("test", "test","test")
-    fernet = Fernet(master_key())
+    FERNET = Fernet(master_key())
     try:
         show_elements()
         print("Enter")
@@ -46,7 +47,7 @@ def start(enter_password):
                 # и только потом закрывался
 
 def create_salt():
-    if not path_to_salt.exists():
+    if not PATH_TO_SALT.exists():
         salt = os.urandom(16)
         with open("salt.key", 'wb') as file:
             file.write(salt)
@@ -60,32 +61,32 @@ def master_key():
         length=32,
         algorithm=hashes.SHA256(),
     )
-    key = base64.urlsafe_b64encode(kdf.derive(master_password.encode()))
+    key = base64.urlsafe_b64encode(kdf.derive(MASTER_PASSWORD.encode()))
     return key
 
 def create_element(link, login, password):
-    link_binary = fernet.encrypt(link.encode())
-    login_binary = fernet.encrypt(login.encode())
-    password_binary = fernet.encrypt(password.encode())
+    link_binary = FERNET.encrypt(link.encode())
+    login_binary = FERNET.encrypt(login.encode())
+    password_binary = FERNET.encrypt(password.encode())
     for element in show_elements():
         if link and login and password in element:
             print("This element already added")
             return
-    cursor.execute("INSERT INTO vault (link, login, password) VALUES (?,?,?)",
+    CURSOR.execute("INSERT INTO vault (link, login, password) VALUES (?,?,?)",
                 (sqlite3.Binary(link_binary),
                             sqlite3.Binary(login_binary),
                             sqlite3.Binary(password_binary)))
-    conn.commit()
+    CONN.commit()
 
 def show_elements (decrypt = True):
-    elements = cursor.execute("SELECT * FROM vault").fetchall()
+    elements = CURSOR.execute("SELECT * FROM vault").fetchall()
     elements_decrypt = []
     index = 0
     for record in elements:
         elements_decrypt.append([])
         for element in record:
             if decrypt:
-                elements_decrypt[index].append(fernet.decrypt(element).decode())
+                elements_decrypt[index].append(FERNET.decrypt(element).decode())
             if not decrypt:
                 elements_decrypt[index].append(element)
         index += 1
@@ -94,29 +95,29 @@ def show_elements (decrypt = True):
 def show_element_secret_data(index, decrypt = True, show_index = False):
     return show_elements(decrypt)[index]
 def delete_element(index):
-    cursor.execute("DELETE FROM vault WHERE link = ? AND login = ? AND password = ?"
+    CURSOR.execute("DELETE FROM vault WHERE link = ? AND login = ? AND password = ?"
                    , (show_element_secret_data(index, False)[0],
                    show_element_secret_data(index, False)[1],
                       show_element_secret_data(index, False)[2]))
-    conn.commit()
+    CONN.commit()
     print("done")
 
 def update_element(index, new_link, new_login, new_password):
-    cursor.execute("UPDATE vault set "
+    CURSOR.execute("UPDATE vault set "
                    "link = ?, login = ?, password = ? "
                    "WHERE link = ? AND login = ? AND password = ?", (
-                    fernet.encrypt(new_link.encode()),
-                    fernet.encrypt(new_login.encode()),
-                    fernet.encrypt(new_password.encode()),
+                    FERNET.encrypt(new_link.encode()),
+                    FERNET.encrypt(new_login.encode()),
+                    FERNET.encrypt(new_password.encode()),
                     show_element_secret_data(index, False)[0],
                     show_element_secret_data(index, False)[1],
                     show_element_secret_data(index, False)[2]
     ))
-    conn.commit()
+    CONN.commit()
     print("done")
 
 def db_close():
-    conn.close()
+    CONN.close()
 
 def find_element (link):
     index = 0
@@ -165,13 +166,13 @@ def generate_password (amount, lower_case = False,
 
 
 #start("password123")
-generate_password(10, True, False, True, )
+#generate_password(10, True, False, True, )
 #print(find_element("a"))
 #update_element(1, "Apple", "Tim Cock", "orange135")
 #delete_element(4)
 #create_salt()
 #print(show_elements())
-#print("NVIDEA" in show_element_secret_data(4))
+#print("NVIDEA" in show_element_secret_data(4)
 #create_element("NVIDEA", "chinese dude", "pi333")
 #print(show_elements())
 #conn.close()
