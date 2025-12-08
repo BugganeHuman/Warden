2218ccc90ca95e4dc0ba0300aef98410f63d6d8a
Elisei <markizdelakarabas@gmail.com>
2025-12-08 13:59:32 +0500

    Update logics.py

--- logics.py
+++ logics.py
@@ -9,7 +9,6 @@ import string
 import secrets
 import pyperclip
 
-
 CONN = sqlite3.connect("vault.db")
 CURSOR = CONN.cursor()
 MASTER_PASSWORD = None
@@ -91,75 +90,8 @@ def master_key():
     key = base64.urlsafe_b64encode(kdf.derive(MASTER_PASSWORD.encode()))
     return key
 
-def create_element(link, login, password):
-    link_binary = FERNET.encrypt(link.encode())
-    login_binary = FERNET.encrypt(login.encode())
-    password_binary = FERNET.encrypt(password.encode())
-    for element in show_elements():
-        if link in element and login in element and password in element:
-            print("This element already added")
-            return
-    CURSOR.execute("INSERT INTO vault (link, login, password) VALUES (?,?,?)",
-                (sqlite3.Binary(link_binary),
-                            sqlite3.Binary(login_binary),
-                            sqlite3.Binary(password_binary)))
-    CONN.commit()
-
-def show_elements (decrypt = True):
-    elements = CURSOR.execute("SELECT * FROM vault").fetchall()
-    elements_decrypt = []
-    index = 0
-    for record in elements:
-        elements_decrypt.append([])
-        for element in record:
-            if decrypt:
-                elements_decrypt[index].append(FERNET.decrypt(element).decode())
-            if not decrypt:
-                elements_decrypt[index].append(element)
-        index += 1
-    return elements_decrypt
-
-def show_element_secret_data(index, decrypt = True):
-    return show_elements(decrypt)[index]
-def delete_element(index):
-    CURSOR.execute("DELETE FROM vault WHERE link = ? AND login = ? AND password = ?"
-                   , (show_element_secret_data(index, False)[0],
-                   show_element_secret_data(index, False)[1],
-                      show_element_secret_data(index, False)[2]))
-    CONN.commit()
-    print("done")
-
-def update_element(index, new_link, new_login, new_password):
-    CURSOR.execute("UPDATE vault set "
-                   "link = ?, login = ?, password = ? "
-                   "WHERE link = ? AND login = ? AND password = ?", (
-                    FERNET.encrypt(new_link.encode()),
-                    FERNET.encrypt(new_login.encode()),
-                    FERNET.encrypt(new_password.encode()),
-                    show_element_secret_data(index, False)[0],
-                    show_element_secret_data(index, False)[1],
-                    show_element_secret_data(index, False)[2]
-    ))
-    CONN.commit()
-    print("done")
-
 def db_close():
     CONN.close()
-
-def find_element (link):
-    index = 0
-    index_of_list_found = 0
-    list_found = []
-    for element in show_elements():
-        finding_link = element[0]
-        result = [chapter for chapter in str(finding_link).lower()
-                  if str(link).lower() in str(finding_link).lower()]
-        if result:
-            list_found.append(show_element_secret_data(index))
-            list_found[index_of_list_found].append(index)
-            index_of_list_found += 1
-        index += 1
-    return list_found
 
 def generate_password (amount, lower_case = False,
                        upper_case = False, numbers = False,
@@ -188,6 +120,7 @@ def generate_password (amount, lower_case = False,
     print(result_list)
     return result_list
 
+
 #start("password123")
 #print(SALT_EXISTS)
 #create_salt()
@@ -198,7 +131,7 @@ def generate_password (amount, lower_case = False,
 #create_salt()
 #print(show_elements())
 #print("NVIDEA" in show_element_secret_data(4)
-#create_element("Amazon", "JB", "dog0101")
+#operations.create_element("Google", "Dude", "zero0101")
 #print(show_elements())
 #conn.close()
 # мб надо логику как то распределить на несколько файлов,
