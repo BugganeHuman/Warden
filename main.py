6083e3f706e3d9ea58950c912c27aefda245d441
Elisei <markizdelakarabas@gmail.com>
2025-12-12 17:20:24 +0500

    Update main.py

--- main.py
+++ main.py
@@ -3,20 +3,16 @@ import logics
 import customtkinter
 
 
-class App(customtkinter.CTk):
-    def __init__(self):
-        super().__init__()
-
-        self.title()
-        self.geometry("1000x800")
-        """
+def main():
     app = customtkinter.CTk()
     app.geometry("1000x800")
     app.title("Warden")
     customtkinter.set_appearance_mode("system")
     customtkinter.set_default_color_theme("green")
+    app.resizable(False, False)
     frame_start = customtkinter.CTkFrame(app)
     frame_vault = customtkinter.CTkFrame(app)
+#___________________________________________________________________
 
     check_entry = customtkinter.CTkEntry(frame_start, placeholder_text="Enter Password",
                             width=500, height=50, font=("Verdana", 44), show="*" )
@@ -27,30 +23,31 @@ class App(customtkinter.CTk):
     check_label = customtkinter.CTkLabel(frame_start, text="", width=300,
                     height=50, font=("Verdana", 44))
     check_entry.bind("<Return>", lambda event : start(check_entry.get()))
-    frame_start.grid_rowconfigure((0, 1), weight=1)
-    frame_start.grid_columnconfigure((0, 1), weight=1)
-    #frame_start.grid_rowconfigure(1, weight=0)
-    #frame_start.grid_columnconfigure(1, weight=1)
+    check_entry.place(x=220, y=350)
+    check_btn.place(x=730, y=352)
+    check_label.place(x=270, y=420)
 
-    check_entry.grid(row=0, column=0, columnspan=2 ) # не работает нихуя
-    check_btn.grid(row=0, column=1 )
-    check_label.grid(row=1, column=0 )
-    #check_btn.place(x=730, y=352)
-    #check_label.place(x=320, y=420)
+
 
     #______________________________________________________________
     generate_password_btn = customtkinter.CTkButton(frame_vault,
             text="generate\npassword", width=150, height=50, font=("Verdana", 30),
                                     fg_color="forestgreen", text_color="ivory" )
-    generate_password_btn.grid(row=1, column=20, padx=500, sticky="n")
-    test_list = ["dog", "cat", "elephant", "squirrel", "bear"]
+    generate_password_btn.place(x=700, y=10)
+
+    elements_frame = customtkinter.CTkScrollableFrame(frame_vault, width=1000, height=900)
+
     index = 0
     row = 0
-    for element in test_list:
-        element_label = customtkinter.CTkLabel(frame_vault, text=element, font= ("Verdana", 25), text_color="ivory")
-        index_label = customtkinter.CTkLabel(frame_vault, text=str(index),font= ("Verdana", 25), text_color="ivory" )
-        element_label.grid(row=row, column=1)
-        index_label.grid(row=row, column=2, padx=10)
+    for element in operations.show_elements():
+        index_label = customtkinter.CTkLabel(elements_frame, text=str(index), font= ("Verdana", 25), text_color="ivory")
+        index_label.grid(row=row, column=0)
+        link_label = customtkinter.CTkLabel(elements_frame, text=element[0], font= ("Verdana", 25), text_color="ivory")
+        link_label.grid(row=row, column=1, padx=10)
+        login_label = customtkinter.CTkLabel(elements_frame, text='*' * len(element[1]), font= ("Verdana", 25), text_color="ivory")
+        login_label.grid(row=row, column=2, padx=10)
+        password_label = customtkinter.CTkLabel(elements_frame, text='*' * len(element[2]), font= ("Verdana", 25), text_color="ivory" )
+        password_label.grid(row=row, column=3, padx=10)
         row += 1
         index += 1
     #______________________________________________________________
@@ -73,13 +70,13 @@ class App(customtkinter.CTk):
     def show_vault_frame(): # надо будет заполнить его
         frame_start.pack_forget()
         frame_vault.pack(fill="both", expand=True)
+        elements_frame.place(x=0, y=100)
     show_start_frame()
 
     app.mainloop()
 
 #_____________________________________________________________________
-"""
+
 if __name__ == "__main__":
-    app = App()
-    app.mainloop()
+    main()
     logics.db_close()
